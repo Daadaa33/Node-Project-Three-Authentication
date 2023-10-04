3ba55bd5f0e1b037aea07cc463f0e415cc1e9639
daadaa <wlaaldaadaa33@gmail.com>
2023-10-04 11:59:35 +0300

    adding connect supabase

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Import server here and start the application
+import server from "./api/server.js";
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log(`listening on localhost:${PORT}`);
+});
